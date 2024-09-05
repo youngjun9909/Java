5efc45214f59fdd579105271e983c81f72353892
Youngjun9909 <dydekd7511@gmail.com>
2024-09-05 14:29:26 +0900

    test

---
+++ java_study/src/chapter04/Object01.java
@@ -0,0 +1,10 @@
+package chapter04;
+
+public class Object01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ java_study/src/chapter04/Object02.java
@@ -0,0 +1,10 @@
+package chapter04;
+
+public class Object02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
