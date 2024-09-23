c2aea25974341f8db44184593149857cc4d64a15
Youngjun9909 <dydekd7511@gmail.com>
2024-09-23 09:34:26 +0900

    remove

--- java_study/src/chapter18/Builder02.java
+++ java_study/src/chapter18/Builder02.java
@@ -1,14 +1,25 @@
 package chapter18;
-// 학생 클래스 구현
-// name, age 필수
-// grade, major 선택
+
+// == 빌더 패턴 없이! ==
+// 학생 클래스 구현 
+
+// 1) private한 필드 데이터 설정
+// : name, age (필수 속성)
+// : grade, major (선택적 속성)
+
+// 2) 필수/선택적 값을 받을 생성자 구현
+// >> 생성자 4개 구현
+
+// 3) 데이터 호출을 위한 Getter 구현
+// >> 각 필드에 대한 getter 구현
+
 class Student {
 	private final String name;
 	private final int age;
-	private String grade;
+	private String grade; // 학년
 	private String major;
-
-	// 빌더 패턴 사용 시의 생성자
+	
+	// == 빌더 패턴 사용 시의 생성자 ==
 	private Student(Builder builder) {
 		this.name = builder.name;
 		this.age = builder.age;
@@ -16,11 +27,11 @@ class Student {
 		this.major = builder.major;
 	}
 	
-	// 빌더 클래스 정의
+	// == 빌더 클래스 정의 (내부 클래스) ==
 	public static class Builder {
 		private final String name;
 		private final int age;
-		private String grade;
+		private String grade; // 학년
 		private String major;
 		
 		public Builder(String name, int age) {
@@ -28,7 +39,7 @@ class Student {
 			this.age = age;
 		}
 		
-		// - 선택적 필드의 체닝을 위한 메서드 - 
+		// - 선택적 필드의 체이닝을 위한 메서드 -
 		public Builder grade(String grade) {
 			this.grade = grade;
 			return this;
@@ -38,27 +49,26 @@ class Student {
 			return this;
 		}
 		
-		// 빌더를 통한 Student 인스턴스 생성
+		// - 빌더를 통한 Student 인스턴스 생성
 		public Student build() {
 			return new Student(this);
 		}
 	}
 	
-// 필수 선택적 값을 받을 생성자 구현
-//>> 생성자 4개 구현
+	// 생성자
 //	public Student(String name, int age) {
-//        this.name = name;
-//        this.age = age;
-//    }
-//	
-//	// 생성자의 오버로딩
-//	//: 파라미터의 개수와 타입이 같다면 생성자 호출 시 충돌 발생
+//		this.name = name;
+//		this.age = age;
+//	}
+	
+	// cf) 생성자의 오버로딩
+	// : 파라미터의 개수와 타입이 같다면 생성자 호출 시 충돌 발생
 //	public Student(String name, int age, String selectedField) {
 //		this.name = name;
 //		this.age = age;
 //		this.grade = selectedField;
 //	}
-//	
+//
 //	public Student(String name, int age, String grade, String major) {
 //		this.name = name;
 //		this.age = age;
@@ -66,12 +76,11 @@ class Student {
 //		this.major = major;
 //	}
 	
-// 데이터 호출을 위한 Getter 구현
-//>> 각 필드에 대한 getter 구현
+	// Getter 메서드
 	public String getName() {
 		return name;
 	}
-	public int getAge() {
+	public int getAge () {
 		return age;
 	}
 	public String getGrade() {
@@ -84,18 +93,31 @@ class Student {
 
 public class Builder02 {
 	public static void main(String[] args) {
-//		Student st1 = new Student("박영준", 26, "1학년");
+//		Student st1 = new Student("박영준", 28, "1학년");
 //		Student st2 = new Student("홍길동", 30);
 //		Student st3 = new Student("전우치", 34, "4학년");
-//		System.out.println(st1.getGrade());
-//		System.out.println(st2.getAge());
-//		System.out.println(st3.getName());
+//		System.out.println(st1.getGrade()); // 1학년
+//		System.out.println(st2.getAge()); // 30
+//		System.out.println(st3.getName()); // 전우치
 		
-		Student st1 = new Student.Builder("박영준", 26).grade("1학년").build();
-		Student st2 = new Student.Builder("홍길동", 30).build();
-		Student st3 = new Student.Builder("전우치", 34).grade("4학년").major("컴퓨터").build();
-		System.out.println(st1.getGrade());
-		System.out.println(st2.getAge());
-		System.out.println(st3.getName());
+		Student std1 = new Student.Builder("박영준", 28).build();
+		
+		Student std2 = new Student.Builder("홍길동", 30)
+				.grade("1학년")
+				.major("회계학과")
+				.build();
+		
+		Student std3 = new Student.Builder("전우치", 16)
+				.grade("3학년")
+				.build();
+		
+		Student std4 = new Student.Builder("임꺽정", 19)
+				.major("심리학과")
+				.build();
+		
+		System.out.println(std1.getName());
+		System.out.println(std2.getName());
+		System.out.println(std3.getName());
+		System.out.println(std4.getName());
 	}
 }
